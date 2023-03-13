bb277a2cbe567aed753f1eae48b9b79887777256
Ceciliaolabode <60604394+Ceciliaolabode@users.noreply.github.com>
2023-03-13 11:43:07 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,55 @@
 <?php
- // Side Hustle Assignment ( Week 1)
-    $Cecilia; // declaring my name as a variable
-    echo $Cecilia; // echoing my variable i.e my name
+     // ECHO FUNCTION
+    // echo "<b>Hello World<b>"; 
+
+    // COMMENT
+    //echo "<b>Hello World<b>"; comment
+    #echo "<b>Hello World<b>"; comment
+    /*echo "<b>Hello World<b>";*/  //multi
+
+    // VARIABLES
+    // $msg ="<b>Hello World!<b>"; 
+    // $msg ="<b>Hello Nigeria!<b>";
+    // echo $msg;
+
+    // CONSTANTS
+    // define("MESSAGE", "Hi Side Hustlers");
+    // // define("MESSAGE", "Hi World"); // error
+    // echo MESSAGE;
+
+    // DATATYPES AND OPERATORS
+    // Strings
+    // $String1 = "Hello,";
+    // $String2 = "Welcome to my world.";
+    // echo $String1. ' ' .$String2;
+
+    // Integer
+    // $a = 23;
+    // $b = -25;
+    // echo $a;
+    
+    // Float 
+    // Boolean
+
+    // Operators:
+    // // Arithmetic:(+, -, /, * , % etc)
+    // $num = 45;
+    // $num2 = 12;
+    // echo $num + $num2;
+
+    // // Assignment
+    // $num1 = 27;
+    // $num2 = 10;
+    // $num1 += $num2; // $num1 + $num2  = $num1
+    // echo $num1;
+
+    // Side Hustle Assignment ( Week 1)
+    $name = "Cecilia Olabode"; // declaring my name as a variable
+    echo $name; // echoing my variable i.e my name
+
+    // WEEK 2
+    // Arrays in PHP
+    // Types of Arrays: Numeric Arrays, Associative Arrays, Multi-dimensional Arrays.
+    // $names = array("David", "Amy", "John");
+    // echo $names[0];
 ?>
